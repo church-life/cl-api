bd4f599c1f330d97801fef2f7e0583b0f63d6918
Jhonatan Macazana <46355455+jhonatanmacazana@users.noreply.github.com>
2024-02-02 01:16:12 -0500

    feat: add serviceteam module to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ServiceTeamsModule } from './service-teams/service-teams.module';
 
 @Module({
-  imports: [],
+  imports: [ServiceTeamsModule],
   controllers: [AppController],
   providers: [AppService],
 })
