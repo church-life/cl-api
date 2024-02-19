a2d8e2f7226bc28a1a54e5fe44df73edea719991
Jhonatan Macazana <46355455+jhonatanmacazana@users.noreply.github.com>
2024-02-19 10:48:36 -0500

    feat: update create ministry dto

--- src/ministries/dto/create-ministry.dto.ts
+++ src/ministries/dto/create-ministry.dto.ts
@@ -1,1 +1,24 @@
-export class CreateMinistryDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { type Prisma } from '@prisma/client';
+
+export class CreateMinistryDto implements Prisma.MinistryCreateInput {
+  @ApiProperty({
+    description: 'The name of the ministry',
+    example: 'Youth',
+  })
+  name: string;
+
+  @ApiProperty({
+    description: 'The minimum age for the ministry',
+    example: 21,
+    required: false,
+  })
+  minAge?: number | null | undefined;
+
+  @ApiProperty({
+    description: 'The maximum age for the ministry',
+    example: 35,
+    required: false,
+  })
+  maxAge?: number | null | undefined;
+}
